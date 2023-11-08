d30dd81fc84df4fb7a1aba6f0906bebed7df53c4
mawudem <koledziabel@gmail.com>
2023-11-08 21:34:04 +0100

    la fin du deplacement du projet du depot creé par anna vers le depot que je vien de creer en conservant la version de l'ancienne projet

--- takeoff.js
+++ takeoff.js
@@ -1,5 +1,5 @@
 var colors = require("colors");
-var unsafeRequire = require("./utils/unsafeRequire");
+var unsafeRequire = require("./takeOffTools/utils/unsafeRequire");
 
 // Pour rendre node un peu plus souple pour l'exercice (ne jamais utiliser ailleurs)
 var unsafeRequire = function(m, stub){
@@ -24,26 +24,26 @@ var stub = {
 
 var engine = unsafeRequire("./engine", stub);
 var command = unsafeRequire("./command", stub);
-var radio = unsafeRequire("./radio", stub);
-var satellite1 = unsafeRequire("./satellite1", stub);
-var satellite2 = unsafeRequire("./satellite2", stub);
+//var radio = unsafeRequire("./radio", stub);
+//var satellite1 = unsafeRequire("./satellite1", stub);
+//var satellite2 = unsafeRequire("./satellite2", stub);
 
 
 
 var rocket = {
 	engine: engine, 
 	command: command, 
-	radio: radio, 
-	satellite1: satellite1,
-	satellite2: satellite2
+	//radio: radio, 
+//	satellite1: satellite1,
+//	satellite2: satellite2
 };
 
 var checklist = {
 	engine: 0, 
 	command: 0, 
-	radio: 0, 
-	satellite1: 0,
-	satellite2: 0
+	//radio: 0, 
+	//satellite1: 0,
+	//satellite2: 0
 };
 
 var takeOff = function(){
