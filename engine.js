72d7931b164b8403085a932848460757d2b355a5
HanyuZZ <101200200+HanyuZZ@users.noreply.github.com>
2023-11-15 15:48:05 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -2,6 +2,7 @@ var Engine = {
 check: function(){
     console.log("Engine Ready !")
     console.log("Blue level checked".blue)
+    console.log("Magenta level checked".magenta)
     return true
     }
 }
