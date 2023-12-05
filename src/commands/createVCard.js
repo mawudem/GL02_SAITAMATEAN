2d99cbbdb02b589198a0ba436a801486b41a560e
Anna <anna.valier@utt.fr>
2023-12-05 19:15:41 +0100

    ajout createVCard.js

---
+++ src/commands/createVCard.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")();
+let choice = prompt("Nom");
+console.log(choice)
+
+let choicep = prompt("Prénom");
+console.log(choicep)
+
+let choicet = prompt("Téléphone");
+console.log(choicet)
+
+let choicee = prompt("Email");
+console.log(choicee)
